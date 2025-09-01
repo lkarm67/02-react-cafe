71943658cc7a6b8e3e6cf131771ac8cf93a41227
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-09-01 11:52:13 +0300

    add code to components, add new file global.d.ts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import css from './App.module.css';
 import CafeInfo from "../CafeInfo/CafeInfo";
-import { Votes, VoteType } from "../../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 export default function App() {
-  // стан з голосами
   const [votes, setVotes] = useState<Votes>({
     good: 0,
     neutral: 0,
     bad: 0,
   });
 
-  // обробка голосування
   const handleVote = (type: VoteType) => {
     setVotes(prevVotes => ({
       ...prevVotes,
@@ -19,17 +20,29 @@ export default function App() {
     }));
   };
 
-  // скидання голосів
   const resetVotes = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
 
-
-  
+  const totalVotes = votes.good + votes.neutral + votes.bad;
   
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
