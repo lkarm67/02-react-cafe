dc3b2eadb178a442ca128af3eb90d887200f64f4
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-09-01 12:14:45 +0300

    check vercel

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -3,7 +3,7 @@ import css from "./CafeInfo.module.css";
 export default function CafeInfo() {
     return (
         <div className={css.container}>
-          <h1 className={css.title}>Sip Happens Café</h1>
+          <h1 className={css.title}>Sip Happens Café/trying Vercel</h1>
             <p className={css.description}>
                 Please rate our service by selecting one of the options below.
             </p>
